7b96ab68f1d53c03f1be23ae3e277f3fd621e6ec
OdeliaFink <odelia.finkel@gmail.com>
2023-07-27 11:52:43 -0400

    initial commit

---
+++ src/components/SuperHeroes.page.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const SuperHeroesPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/superheroes');
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        // Handle errors, e.g., set a state variable to display an error message.
+        console.error('Error fetching data:', error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>;
+      })}
+    </>
+  );
+};
